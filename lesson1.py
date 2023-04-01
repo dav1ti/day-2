5f20c7ca23fe8a26457651d458d3520c554eedb9
dav1ti <davidanakidze@gmail.com>
2023-04-01 14:54:04 +0400

    day 2

---
+++ lesson1.py
@@ -0,0 +1,87 @@
+from turtle import*
+width (7)
+color("purple")
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+#end of square
+#drawing a door
+left(90)
+forward(100)
+color("red")
+left(90)
+forward(90)
+right(90)
+forward(70)
+right(90)
+forward(90)
+exitonclick
+#end of door
+#roof
+color("purple")
+left(90)
+forward(30)
+left(90)
+forward(200)
+left(45)
+forward(140)
+left(90)
+forward(140)
+#end of roof
+#windows
+left(45)
+forward(200)
+left(90)
+exitonclick
+forward(40)
+left(90)
+color("black")
+forward(60)
+right(90)
+forward(40)
+right(90)
+forward(60)
+exitonclick
+right(90)
+forward(20)
+right(90)
+forward(60)
+left(90)
+forward(20)
+left(90)
+forward(40)
+left(90)
+forward(40)
+right(90)
+forward (20)
+right(90)
+forward(40)
+#end of the door
+#door handware
+left(90)
+left(90)
+forward(40)
+color("purple")
+forward(20)
+color("red")
+forward(70)
+left(90)
+forward(45)
+left(90)
+forward(10)
+left(90)
+forward(5)
+left(90)
+forward(10)
+left(90)
+forward(50)
+exitonclick
+#end
